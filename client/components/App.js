7933bca7e7b7b403f67a6da6eebf4347c071032b
Andrey <fi93@mail.ru>
2016-09-28 23:12:43 +0200

    create smart component (Accessing Dispatch and State with Redux)

---
+++ client/components/App.js
@@ -0,0 +1,22 @@
+/**
+ * Andrei Fidelman
+ * 28/09/16
+ * 23:03
+ */
+import { connect } from 'react-redux';
+import Main from './Main';
+import { bindActionCreators } from 'redux';
+import * as AC from '../actions/actionCreators';
+
+function mapStateToProps(state) {
+    const { posts, comments }= state;
+    return { posts, comments};
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(AC, dispatch);
+}
+
+const App = connect(mapStateToProps, mapDispatchToProps)(Map);
+
+export default App;
