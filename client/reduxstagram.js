6e77fd80f4ee15a0094dc98d00e312c3fc21d643
Andrey <fi93@mail.ru>
2016-09-28 23:02:12 +0200

    add root component

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,36 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+// Import CSS
+import css from './styles/style.styl';
+
+// Import Components
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+// Import react-router dep
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+
+
+const router = (
+    <Router history={history}>
+        <Route path="/" component={Main}>
+            <IndexRoute component={PhotoGrid} />
+            <Route path ="/view/:postId" component={Single} />
+        </Route>
+    </Router>
+);
+
+
+
+
+
+render(
+    <Provider store={store}>
+        {router}
+    </Provider>
+    , document.getElementById('root'));
