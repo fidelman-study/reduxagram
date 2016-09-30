73b40803fb4bd95d96352d95546a7ac78273f842
Andrey <fi93@mail.ru>
2016-09-30 19:20:06 +0200

    connect to Sentry.io

--- client/components/Main.js
+++ client/components/Main.js
@@ -6,13 +6,33 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 
+import Raven from 'raven-js';
+import { sentry_url, logException } from '../data/config';
+
+
 class Main extends Component {
+
+    handleError() {
+        Raven.config(sentry_url, {
+            tags: {
+                git_commit: 'as09d8f09'
+            }
+        }).install();
+
+        logException(new Error('Incomplete Data!'), {
+            email: 'wesbos@gmail.com'
+        });
+
+        Raven.showReportDialog();
+    }
+
     render() {
         return (
             <div>
                 <h1>
                     <Link to="/">Reduxstagram</Link>
                 </h1>
+                <button onClick={this.handleError.bind(this)}>Error</button>
                 {React.cloneElement(this.props.children, this.props)}
             </div>
         );
--- client/data/config.js
+++ client/data/config.js
@@ -1,8 +1,8 @@
 import Raven from 'raven-js';
 
-const sentry_key = 'cb55d4f05cd443ce82303222f77ef5e0';
-const sentry_app = '61499';
-export const sentry_url = `https://${sentry_key}@app.getsentry.com/${sentry_app}`;
+const sentry_key = 'b40977084ab547ed9e00a548790ccb0c';
+const sentry_app = '102631';
+export const sentry_url = `https://${sentry_key}@sentry.io/${sentry_app}`;
 
 export function logException(ex, context) {
   Raven.captureException(ex, {
