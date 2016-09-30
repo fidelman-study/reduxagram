9481e190bb22ad4a4a8aca3b0ec2c9716a4224ac
Andrey <fi93@mail.ru>
2016-09-30 18:45:24 +0200

    create Single component

--- client/components/Single.js
+++ client/components/Single.js
@@ -4,12 +4,21 @@
  * 19:33
  */
 import React, { Component, PropTypes } from 'react';
+import Photo from './Photo';
 
 class Single extends Component {
     render() {
+
+        const { posts, comments, params:{ postId } } = this.props;
+
+        const i = posts.findIndex(post => post.code === postId);
+        const post = posts[i];
+
+        const postComments = comments[postId] || [];
+
         return (
             <div className="single-photo">
-                i'm single
+                <Photo i={i} post={post} {...this.props} />
             </div>
         );
     };
